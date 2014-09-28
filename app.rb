c9224911404d921c37e2474ff20294bdc9aa90b3
Tanya Filonych <tfilonych@mail.ru>
2014-09-28 17:25:43 +0300

    implemented registration and login(TODO: make it nice in future commits)

--- app.rb
+++ app.rb
@@ -16,6 +16,32 @@ get '/' do
   send_file 'public/index.html'
 end
 
+post '/register' do
+  data = JSON.parse request.body.read
+  user = User.new
+  user.username = data['username'] 
+  user.first_name = data['first_name']
+  user.last_name = data['last_name']
+  user.hashed_password = data['password']
+  user.email = data['email']
+  user.birthday = data['birthday']
+  user.plast_hovel = data['plast_hovel']
+  user.plast_region = data['plast_region']
+  user.plast_level = data['plast_level']
+  user.picture = data['picture']
+  user.save
+  return [200, "ok"]
+end
+
+post '/login' do
+  data = JSON.parse request.body.read
+  user = User.where(username: data['username'], hashed_password: data['password']).first 
+  if !user.nil?
+    return [200, "ok"]
+  end
+    return [401, "unauthorized"]
+end
+
 not_found do
   send_file 'public/index.html'
 end
--- public/js/quiz.js
+++ public/js/quiz.js
@@ -4,11 +4,11 @@
 	app.config(['$routeProvider', "$locationProvider", function($routeProvider, $locationProvider) {
 		$locationProvider.html5Mode(true);
 	    $routeProvider
-	    	.when('/main', {templateUrl: '/templates/main.html'})
+	    	.when('/', {templateUrl: '/templates/main.html'})
 	    	.when('/search', {templateUrl: '/templates/search.html'})
 	    	.when('/contacts', {templateUrl: '/templates/contacts.html'})
 	    	.when('/statistics', {templateUrl: '/templates/statistics.html'})
-	    	.when('/registration', {templateUrl: '/templates/registration.html'})
+	    	.when('/registration', {templateUrl: '/templates/registration.html', controller: "RegistrationController", controllerAs: "reg"})
 	      	.otherwise({redirectTo: '/'});
 	}]);
   
@@ -16,17 +16,40 @@
 		
 	});
 	
-	app.controller("LoginController", ["$location", function($location){
-		
-		this.username = "";
-		this.password = "";
+	app.controller("RegistrationController", ["$http", "$location", function($http, $location){
+		this.user = {};
+		this.cancel = function(){
+			$location.path("/");
+		};
+		this.submitRegistration = function(){
+			$http.post("/register", this.user)
+			.success(function(data){
+				$location.path("/");
+			});
+		};
+	}]);
+	
+	app.controller("LoginController", ["$location", "$http", function($location, $http){
+		var lgnCtrl = this;
+		this.user = {};
+		this.message = "";
 		this.submitLogin = function(){
-			
+			if (!this.user.username || !this.user.password){
+				this.message = "Enter username and password!";
+			} else {
+				$http.post("/login", this.user)
+					.success(function(data){
+						$("#login").modal("hide");
+						$location.path("/");
+					})
+					.error(function(data){
+						lgnCtrl.message = "Invalid username and/or password!";
+					});
+			}
 		};
 		this.register = function(){
 			$("#login").modal("hide");
 			$location.path("/registration");
-			
 		};
 	}]);
 	
@@ -47,10 +70,5 @@
 			controllerAs: "nav"
 		};
 	});
-	
-	
-	
-	
-	
 	
 })();
